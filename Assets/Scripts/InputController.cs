c09c38ab3cb4677efa76bafaba3ad963d944b78b
sjdodge123 <sjdodge@outlook.com>
2016-12-07 21:33:54 -0500

    --Added remove ball function --Added right click to reset game --Added Level 3

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour
+{
+    public GameObject spawnObject;
+    private CircleCollider2D spawnCollider;
+    // Use this for initialization
+    void Start()
+    {
+        spawnCollider = spawnObject.GetComponent<CircleCollider2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (LevelManager.GetCurrentLevelNumber() == 0)
+            {
+                LevelManager.LoadFirstLevel();
+                return;
+            }
+            //Spawn Gravity Well
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 1;
+            if (CollidesWithSpawnObject(mousePos, spawnCollider.radius))
+            {
+                return;
+            }
+            var gravWell = Instantiate(spawnObject, mousePos, Quaternion.identity) as GameObject;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            LevelManager.ResetCurrentLevel();
+        }
+    }
+    private bool CollidesWithSpawnObject(Vector3 pos,float radius)
+    {
+        Collider2D[] collisions = Physics2D.OverlapCircleAll(pos, radius);
+        if (collisions.Length > 0)
+        {
+            foreach (Collider2D collision in collisions)
+            {
+                if (collision.gameObject.CompareTag(spawnObject.tag))
+                {
+                    Destroy(collision.gameObject);
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/Level3Rules.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Level3Rules : MonoBehaviour {
+
+    public GameObject friendBall;
+    private Rigidbody2D ballBody;
+	// Use this for initialization
+	void Start () {
+        ballBody = friendBall.GetComponent<Rigidbody2D>();
+        ballBody.velocity = new Vector2(1,0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
